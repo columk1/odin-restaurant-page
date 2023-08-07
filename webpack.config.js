a2c44883535b9325657e0092cc8f7ec5cad15581
Colum Kelly <columk1@users.noreply.github.com>
2023-08-06 19:41:29 -0700

    Add build folder for Netlify

--- webpack.config.js
+++ webpack.config.js
@@ -13,8 +13,8 @@ module.exports = {
     }),
   ],
   output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'build'),
     clean: true,
   },
   module: {
